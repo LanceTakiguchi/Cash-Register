3cced1b7f6bc5c3a4d95b6c9b685b333ceadb624
Lance Takiguchi <lancetakiguchi@gmail.com>
2017-08-29 10:19:22 -0700

    banana button prints

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+// Product buttons
+var products = [{name: "Banana", price: 0.69}, {name: "Whole Milk Gallon", price: 2.99}, {name: "Potato Chips", price: 1.59}];
+var banana = {name: "Banana", price: 0.69};
+
+$(document).ready(function() {
+      $(".banana").click(function(){
+      	console.log("Hello world")
+      });
+});
