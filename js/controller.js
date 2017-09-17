19729f0fa3ed1f9fef8bb893dd588fa24fc46c60
Lance Takiguchi <lancetakiguchi@gmail.com>
2017-09-16 18:32:20 -0700

    button function adds product to the bill

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,22 @@ var banana = {name: "Banana", price: 0.69};
 var grocery_list = [];
 var recall_number = function(index){
 	return function(){
-		alert(index);
+		grocery_list.push(products[index]);
+		console.log(grocery_list);
+		add_item(products[index]);
 	}
 }
+var add_item = function(product){
+		var listing = '<tr><td> ';
+		listing += product.name; 
+		listing += ' </td><td> '
+		listing += product.price;
+		listing += ' </td><td> 1 </td><td> '
+		listing += product.price;
+		listing += ' </td><td class="trash"> <button class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button> </td></tr>';
+		var item = $(listing);
+		item.appendTo(".bill");
+	}
 $(document).ready(function() {
 	for(var item in products){
 		var button = $('<button class="btn btn-outline-info">'+ products[item].name + ' | $' + products[item].price + '</button>');
