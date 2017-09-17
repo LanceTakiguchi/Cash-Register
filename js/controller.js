dccf33cfa20338577a023c665b03cfd5251c3422
Lance Takiguchi <lancetakiguchi@gmail.com>
2017-09-16 17:39:24 -0700

    working: product number saved in alert

--- js/controller.js
+++ js/controller.js
@@ -2,19 +2,15 @@
 var products = [{name: "Banana", price: 0.69}, {name: "Whole Milk Gallon", price: 2.99}, {name: "Potato Chips", price: 1.59}];
 var banana = {name: "Banana", price: 0.69};
 var grocery_list = [];
-/*var add_to_list = function(item){
-	grocery_list.push(products[item]);
-	console.log(grocery_list);
-}*/
-var add_to_list = function(){
-	console.log("Hello");
+var recall_number = function(index){
+	return function(){
+		alert(index);
+	}
 }
 $(document).ready(function() {
 	for(var item in products){
 		var button = $('<button class="btn btn-outline-info">'+ products[item].name + ' | $' + products[item].price + '</button>');
-		button.click(function(){
-			console.log("Hello");
-		});
+		button.click(recall_number(item));
 		button.appendTo(".products");
 	}
 });
